44ae743cc070e001ed869cacf54d36784b618978
Yashiry Azize <azizeyashiry@gmail.com>
2021-06-16 22:05:57 -0400

    complete newlogform setup

--- frontend-budgeting-app/src/Components/NewLogForm.js
+++ frontend-budgeting-app/src/Components/NewLogForm.js
@@ -8,11 +8,53 @@ const NewLogForm = (props) => {
     amount: 0,
   });
 
+  const handleChange = (e) => {
+    setTransaction({...transaction, [e.target.id]: e.target.value})
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-  <section>
-      
-  </section>
-  )
+    <section>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="from">From:</label>
+        <input
+          id="from"
+          value={transaction.from}
+          onChange={handleChange}
+          type="text"
+          placeholder="From..."
+        />
+        <label htmlFor="date">Date:</label>
+        <input
+          id="date"
+          value={transaction.date}
+          onChange={handleChange}
+          type="text"
+          placeholder="Date..."
+        />
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          value={transaction.name}
+          onChange={handleChange}
+          type="text"
+          placeholder="Name..."
+        />
+        <label htmlFor="amount">Amount:</label>
+        <input
+          id="amount"
+          value={transaction.amount}
+          onChange={handleChange}
+          type="text"
+          placeholder="Amount..."
+        />
+        <input type="submit"/>
+      </form>
+    </section>
+  );
 };
 
 export default NewLogForm;
