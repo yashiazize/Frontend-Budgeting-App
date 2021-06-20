6934c14ddef0b1f3a5ed0f05b03694a24907c28a
Yashiry Azize <azizeyashiry@gmail.com>
2021-06-20 11:48:34 -0400

    updated url

--- frontend-budgeting-app/src/utl/apiURL.js
+++ frontend-budgeting-app/src/utl/apiURL.js
@@ -1,10 +1,9 @@
 export const apiURL = () => {
-    if (
-        process.env.NODE_ENV === "development" ||
-        process.env.NODE_ENV === "test"
-      ) {
-        return "http://localhost:3222";
-      } else {
-        return "https://evening-hollows-03778.herokuapp.com/";
-      }
-}
+  if (
+    process.env.NODE_ENV === "development" ||
+    process.env.NODE_ENV === "test"
+  ) {
+    return "http://localhost:3222";
+  }
+  return "https://evening-hollows-03778.herokuapp.com/";
+};
